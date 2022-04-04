1984a57d683b0afc1389f12e2ef031690406fbd5
lucasfarmwald <farmwaldlucas@gmail.com>
2022-04-04 14:28:52 -0500

    innit commit

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+
+    attr_accessor :name, :artist, :genre
+    @@count = 0,
+    @@artists,
+    @@genres
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+
+    end
+    def self.count
+        @@count
+    end
+    def name
+        @name
+    end
+    def artist
+        @artist
+    end
+    def genre
+        @genre
+    end
+    
+end
